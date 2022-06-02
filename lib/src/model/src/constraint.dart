1d23c10d02291ba85912c35044d3b20d897a1072
Felix Weickert <felix.weickert@dws.co.at>
2022-06-03 00:42:46 +0200

    doc: improved documentation

--- lib/src/model/src/constraint.dart
+++ lib/src/model/src/constraint.dart
@@ -4,21 +4,30 @@ import 'table_property.dart';
 ///
 /// *Constraints can be named which facilitates later migrations.*
 abstract class Constraint {
+  /// Define constraint name
+  ///
+  /// Results in following text being put before constraint `CONSTRAINT $name`
   String? name;
-  String get constraint;
 
-  String get constraintWithName {
-    return "CONSTRAINT \"$name\" $constraint";
+  /// Returns sql snippet without the name for this [Constraint]
+  String get _constraintWithoutName;
+
+  /// Returns sql snippet including the name for this [Constraint]. Name might be null.
+  String get _constraintWithName {
+    return "CONSTRAINT \"$name\" $_constraintWithoutName";
   }
 
-  String get fullConstraint {
-    return name == null ? constraint : constraintWithName;
+  /// Returns final sql snippet for this [Constraint]
+  String get constraint {
+    return name == null ? _constraintWithoutName : _constraintWithName;
   }
 
   Constraint({this.name});
 }
 
 /// Possibility of inserting check constraint
+///
+/// By setting this constraint more complex checks comparing column value(s) can be defined.
 ///
 /// Adding e.g. a CheckConstraint with `check="count<10"` will result in an added constraint of `CHECK (count < 10)`
 class CheckConstraint extends Constraint implements TableProperty {
@@ -27,24 +36,27 @@ class CheckConstraint extends Constraint implements TableProperty {
   CheckConstraint({required this.check, super.name});
 
   @override
-  String get constraint => "CHECK($check)";
+  String get _constraintWithoutName => "CHECK($check)";
 
   @override
-  String get sqlSnippet => fullConstraint;
+  String get sqlSnippet => constraint;
 }
 
 /// Class for defining `UNIQUE` constraints.
+///
+/// After setting this constraint there must not be any duplicate values for a column
 ///
 /// In case it should be created as a [TableProperty] the [columnNames] must be provided
 ///
 /// *In case a single column should be marked as unique, without the need of naming the constraint, the `isUnique` flag of the [COLUMN] can be set.*
 class UniqueConstraint extends Constraint implements TableProperty {
+  /// Combination of columns which should be unique.
   List<String>? columnNames;
 
   UniqueConstraint({super.name, this.columnNames});
 
   @override
-  String get constraint {
+  String get _constraintWithoutName {
     final cleanColumnNames = columnNames?.map((name) => "\"$name\"").toList();
 
     // creates " (\"name1\", \"name2\", \"name3\")" if columnNames != null
@@ -55,7 +67,7 @@ class UniqueConstraint extends Constraint implements TableProperty {
   }
 
   @override
-  String get sqlSnippet => fullConstraint;
+  String get sqlSnippet => constraint;
 }
 
 /// Class for defining any constraint.
@@ -64,11 +76,12 @@ class UniqueConstraint extends Constraint implements TableProperty {
 ///
 /// Depending on the used context it will be either added as parameter or appended to the column declaration
 class ManualConstraint extends Constraint implements TableProperty {
+  /// Enter the desired constraint in plain SQL. E.g: `EXCLUDE USING gist (c with &&)`
   String constraintString;
   ManualConstraint(this.constraintString);
 
   @override
-  String get constraint => constraintString;
+  String get _constraintWithoutName => constraintString;
 
   @override
   String get sqlSnippet => constraintString;
@@ -76,16 +89,19 @@ class ManualConstraint extends Constraint implements TableProperty {
 
 /// Class for defining primary keys.
 ///
+/// Defines a column / multiple columns as unqiue identifier of a row
+///
 /// In case it should be created as a [TableProperty] the [columnNames] must be provided
 ///
 /// *In case a single column should be marked as primary key without the need of naming the constraint, the `isPrimaryKey` flag of the column can be directly set.*
 class PrimaryKeyConstraint extends Constraint implements TableProperty {
+  /// Combination of columns which should be unique & identify a row.
   List<String>? columnNames;
 
   PrimaryKeyConstraint({super.name, this.columnNames});
 
   @override
-  String get constraint {
+  String get _constraintWithoutName {
     final cleanColumnNames = columnNames?.map((name) => "\"$name\"").toList();
 
     // creates " (\"name1\", \"name2\", \"name3\")" if columnNames != null
@@ -96,29 +112,49 @@ class PrimaryKeyConstraint extends Constraint implements TableProperty {
   }
 
   @override
-  String get sqlSnippet => fullConstraint;
+  String get sqlSnippet => constraint;
 }
 
 /// Class for defining foreign keys.
 ///
 /// In case the foreign key references a column marked as primary key the referencedColumn can be left blank
 ///
-/// In case it should be created as a [TableProperty] use the [CombinedForeignKeyConstraint] instead.
+/// In case it should be created as a [TableProperty] use the [ForeignKeyConstraint.tableProperty] constructor instead.
+///
+/// In case multiple columns should be combined to be a foreign key use the [ForeignKeyConstraint.multiColumn] constructor instead. If this constructor is used only usage as [TableProperty] is allowed by the DB.
 ///
 /// *In case a single column should be marked as a foreign key the [foreignKeyForTable] property can be set if*
 /// * *The column references a primary key*
 /// * *No deletion or update mode needs to be defined*
 /// * *No name for the foreign key constraints needs to be defined*
 class ForeignKeyConstraint extends Constraint implements TableProperty {
+  /// Specify the column referenced by the foreingKey. Defaults to the [referencedTable]'s primary key
   String? referencedColumn;
+
+  /// The referenced table
   String referencedTable;
+
+  /// How table referencing table rows should be treated on deleting referenced objects.
   ForeignKeyDeletionMode deletionMode;
+
+  /// How table referencing table rows should be treated on opdating referenced objects.
   ForeignKeyUpdateMode updateMode;
+
+  /// Needed to provide correct syntax if not used inline with column.
   bool _isTableProperty = false;
+
+  /// Needed to provide correct syntax if used as [TableProperty] with (multiple) specified column(s).
   bool _isMultiColumn = false;
+
+  /// Needed to be specified if used as [TableProperty] when not used for single column.
   String foreignKeyColumn = "";
+
+  /// Needed to be specified if used as [TableProperty] for multiple columns.
   Map<String, String> columnNames = {};
 
+  /// Constructor for creating foreign keys as inline [Constraint].
+  ///
+  /// Results in sql snippet like e.g. `REFERENCES product_groups`
   ForeignKeyConstraint({
     super.name,
     this.referencedColumn,
@@ -159,7 +195,7 @@ class ForeignKeyConstraint extends Constraint implements TableProperty {
         _isMultiColumn = true;
 
   @override
-  String get constraint {
+  String get _constraintWithoutName {
     String columnReference =
         referencedColumn != null ? " (\"${referencedColumn!}\")" : "";
     String tablePropertySnippet =
@@ -180,7 +216,7 @@ class ForeignKeyConstraint extends Constraint implements TableProperty {
   }
 
   @override
-  String get sqlSnippet => fullConstraint;
+  String get sqlSnippet => constraint;
 }
 
 enum ForeignKeyDeletionMode {
