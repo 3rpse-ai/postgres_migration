dc4fbd8b54165a33634414957dcaabe679691430
Felix Weickert <felix.weickert@dws.co.at>
2022-06-03 09:36:19 +0200

    refactored default value & added documentation

--- lib/src/model/src/column.dart
+++ lib/src/model/src/column.dart
@@ -1,71 +1,134 @@
 import 'constraint.dart';
 import 'table_property.dart';
 
-abstract class Column implements TableProperty {
+abstract class Column<T> implements TableProperty {
   String name;
   String get type;
 
   @override
-  String get sqlSnippet => "\"$name\" $type$constraintsString";
+  String get sqlSnippet =>
+      "\"$name\" $type$_constraintsSnippet$_defaultValueSnippet";
 
+  /// Used to set check constraint.
   CheckConstraint? checkConstraint;
 
+  /// Used to set manual constraint. Allows for freely defining any constraint.
   ManualConstraint? manualConstraint;
 
+  /// Used to set unique constraint. If value is provided [isUnique] flag is ignored.
   UniqueConstraint? uniqueConstraint;
 
+  /// Used to set foreign key constraint. If value is provided [foreignKeyForTable] shorthand is ignored.
   ForeignKeyConstraint? foreignKeyConstraint;
 
+  /// Used to set primary key constraint. If value is provided [isPrimaryKey] flag is ignored.
   PrimaryKeyConstraint? primaryKeyConstraint;
 
+  /// Internal property for retrieving check constraint
+  CheckConstraint? get _checkConstraint => checkConstraint;
+
+  /// Internal property for retrieving manual constraint
+  ManualConstraint? get _manualConstraint => manualConstraint;
+
+  /// Internal property for retrieving unique constraint.
+  ///
+  /// Returns default [UniqueConstraint] if is [isUnique]is set and [uniqueConstraint] is null.
+  UniqueConstraint? get _uniqueConstraint {
+    return uniqueConstraint ?? (isUnique ? UniqueConstraint() : null);
+  }
+
+  /// Internal property for retrieving foreign key constraint.
+  ///
+  /// Returns default [ForeignKeyConstraint] if [foreignKeyForTable]is set and [foreignKeyConstraint] is null.
+  ForeignKeyConstraint? get _foreignKeyConstraint {
+    return foreignKeyConstraint ??
+        (foreignKeyForTable != null
+            ? ForeignKeyConstraint(referencedTable: foreignKeyForTable!)
+            : null);
+  }
+
+  /// Internal property for retrieving primary key constraint.
+  ///
+  /// Returns default [PrimaryKeyConstraint] if is [isPrimaryKey]is set and [primaryKeyConstraint] is null.
+  PrimaryKeyConstraint? get _primaryKeyConstraint {
+    return primaryKeyConstraint ??
+        (isPrimaryKey ? PrimaryKeyConstraint() : null);
+  }
+
   /// Concatenated sql snippets of constraints
-  String get constraintsString {
+  String get _constraintsSnippet {
     String cString = "";
 
-    if (primaryKeyConstraint != null) {
-      cString = "$cString ${primaryKeyConstraint!.sqlSnippet}";
-    }
-    if (foreignKeyConstraint != null) {
-      cString = "$cString ${foreignKeyConstraint!.sqlSnippet}";
+    if (!isNullable) {
+      cString = "$cString NOT NULL";
     }
 
-    if (uniqueConstraint != null) {
-      cString = "$cString ${uniqueConstraint!.sqlSnippet}";
+    if (_primaryKeyConstraint != null) {
+      cString = "$cString ${_primaryKeyConstraint!.sqlSnippet}";
+    }
+    if (_foreignKeyConstraint != null) {
+      cString = "$cString ${_foreignKeyConstraint!.sqlSnippet}";
     }
 
-    if (checkConstraint != null) {
-      cString = "$cString ${checkConstraint!.sqlSnippet}";
+    if (_uniqueConstraint != null) {
+      cString = "$cString ${_uniqueConstraint!.sqlSnippet}";
     }
-    if (manualConstraint != null) {
-      cString = "$cString ${manualConstraint!.sqlSnippet}";
+
+    if (_checkConstraint != null) {
+      cString = "$cString ${_checkConstraint!.sqlSnippet}";
+    }
+    if (_manualConstraint != null) {
+      cString = "$cString ${_manualConstraint!.sqlSnippet}";
     }
 
     return cString;
+  }
+
+  String get _defaultValueSnippet {
+    String? manualOverDefaultString = manualDefaultValue ??
+        (defaultValue != null ? _defaultValueAsString : null);
+    return manualOverDefaultString != null
+        ? " DEFAULT $manualOverDefaultString"
+        : "";
   }
 
   /// Set this value to allow / prohibit null values. Defaults to not nullable.
   bool isNullable;
 
   /// Set this value to enforce the column using only unique values
+  ///
+  /// If [uniqueConstraint] is provided this flag is ignored.
   bool isUnique;
 
   /// Set this value to define the column as a primary key
+  ///
+  /// If [primaryKeyConstraint] is provided this flag is ignored.
   bool isPrimaryKey;
 
   /// Set this value with the referenced table's name to define the column as a foreign key
+  ///
+  /// If [foreignKeyConstraint] is provided this input is ignored.
   String? foreignKeyForTable;
 
-  /// possibility of inserting manual default statement
+  /// Possibility of inserting manual default statement
   ///
-  /// adding e.g. an IntegerColumn called "count" with default="0" will result in an SQL query "count integer DEFAULT 0"
-  String? manualDefault;
+  /// Adding e.g. an IntegerColumn called "count" with default="0" will result in an SQL query "count integer DEFAULT 0"
+  ///
+  /// Always takes precedence over [defaultValue]
+  String? manualDefaultValue;
 
-  String? defaultValue;
+  /// Overwrite this getter to provide a column specific default value. Otherwise `defaultValue.toString()` is returned.
+  String get _defaultValueAsString => defaultValue.toString();
+
+  /// Used to define a default value. Results in sql snippet `DEFAULT $_defaultValue`.
+  ///
+  /// Override _defaultValueAsString getter to transform value into an sql friendly string. Otherwise `defaultValue.toString()` will be inserted into sql query.
+  final T? defaultValue;
 
   Column(
     this.name, {
     this.isNullable = false,
-    this.manualDefault,
+    this.manualDefaultValue,
     this.isPrimaryKey = false,
     this.isUnique = false,
     this.foreignKeyForTable,
@@ -74,13 +137,14 @@ abstract class Column implements TableProperty {
     this.manualConstraint,
     this.primaryKeyConstraint,
     this.uniqueConstraint,
+    this.defaultValue,
   });
 }
 
-class TextColumn extends Column {
+class TextColumn extends Column<String> {
   TextColumn(
     super.name, {
-    super.manualDefault,
+    super.manualDefaultValue,
     super.checkConstraint,
     super.foreignKeyConstraint,
     super.foreignKeyForTable,
@@ -96,20 +160,19 @@ class TextColumn extends Column {
   String get type => 'text';
 }
 
-class IntegerColumn extends Column {
-  IntegerColumn(
-    super.name, {
-    super.manualDefault,
-    super.checkConstraint,
-    super.foreignKeyConstraint,
-    super.foreignKeyForTable,
-    super.isNullable,
-    super.isPrimaryKey,
-    super.isUnique,
-    super.manualConstraint,
-    super.primaryKeyConstraint,
-    super.uniqueConstraint,
-  });
+class IntegerColumn extends Column<int> {
+  IntegerColumn(super.name,
+      {super.manualDefaultValue,
+      super.checkConstraint,
+      super.foreignKeyConstraint,
+      super.foreignKeyForTable,
+      super.isNullable,
+      super.isPrimaryKey,
+      super.isUnique,
+      super.manualConstraint,
+      super.primaryKeyConstraint,
+      super.uniqueConstraint,
+      super.defaultValue});
 
   @override
   String get type => 'integer';
