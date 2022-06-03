8c754c904853a12fb6ec6be73abb7f9e34984059
Felix Weickert <felix.weickert@dws.co.at>
2022-06-03 09:39:37 +0200

    added column abstract class unit tests

--- test/postgres_migration_test.dart
+++ test/postgres_migration_test.dart
@@ -1,11 +1,15 @@
 import 'package:test/test.dart';
 
+import 'column_unit_tests/column_abstract_unit_tests.dart';
 import 'constraint_unit_tests.dart';
 
 void main() {
   group('Unit Tests', () {
     group('// 1. Constraints //', () {
       executeConstraintUnitTests();
+    });
+    group('// 2. Column Abstract Class //', () {
+      executeColumnAbstractClassUnitTests();
     });
   });
 }
