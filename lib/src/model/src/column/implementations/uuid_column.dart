237025b826eefa26dc3affe90fa0effa1bcbd392
Felix Weickert <felix.weickert@dws.co.at>
2022-06-07 07:47:50 +0200

    added uuid column

---
+++ lib/src/model/src/column/implementations/uuid_column.dart
@@ -0,0 +1,24 @@
+import '../column.dart';
+
+/// Column for storing  Universally Unique Identifiers (UUID) as defined by RFC 4122, ISO/IEC 9834-8:2005, and related standards.
+///
+/// Postgres does not provide any default creation of UUID values, hence this needs to be enabled by installing an extension on the sql server, or the server needs to handle it.
+class UUIDColumn extends Column<String> {
+  @override
+  String get type => 'uuid';
+
+  UUIDColumn(
+    super.name, {
+    super.isNullable = false,
+    super.manualDefaultValue,
+    super.isPrimaryKey = false,
+    super.isUnique = false,
+    super.foreignKeyForTable,
+    super.foreignKeyConstraint,
+    super.checkConstraint,
+    super.manualConstraint,
+    super.primaryKeyConstraint,
+    super.uniqueConstraint,
+    super.defaultValue,
+  });
+}
