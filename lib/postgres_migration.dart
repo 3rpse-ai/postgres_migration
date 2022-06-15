9ce95abf6748b4d033132e02ef93fee40c05b801
Felix Weickert <felix.weickert@dws.co.at>
2022-06-15 07:23:11 +0200

    removed comments

--- lib/postgres_migration.dart
+++ lib/postgres_migration.dart
@@ -1,6 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library postgres_migration;
 
 export 'package:postgres/postgres.dart';
@@ -8,7 +5,3 @@ export 'package:postgres/postgres.dart';
 export 'src/columns/columns.dart';
 
 export 'src/migrators/migrators.dart';
-
-
-
-// TODO: Export any libraries intended for clients of this package.
