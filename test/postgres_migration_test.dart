dd9a936e5ed57bf888cad89018021c957ad5a944
Felix Weickert <felix.weickert@dws.co.at>
2022-06-16 15:01:40 +0200

    moved unit tests into own folder

--- test/postgres_migration_test.dart
+++ test/postgres_migration_test.dart
@@ -1,9 +1,9 @@
 import 'package:test/test.dart';
 
-import 'column_unit_tests/column_abstract_unit_tests.dart';
-import 'column_unit_tests/column_implemenation_unit_tests.dart';
-import 'constraint_unit_tests.dart';
-import 'migrator_unit_tests/migrator_tests.dart';
+import 'unit_tests/column_unit_tests/column_abstract_unit_tests.dart';
+import 'unit_tests/column_unit_tests/column_implemenation_unit_tests.dart';
+import 'unit_tests/constraint_unit_tests.dart';
+import 'unit_tests/migrator_unit_tests/migrator_tests.dart';
 
 void main() {
   group('Unit Tests', () {
