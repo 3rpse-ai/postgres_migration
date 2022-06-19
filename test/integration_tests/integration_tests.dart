304e63d399fb47c9100f531c05e0d3f0f43b1c1a
Felix Weickert <felix.weickert@dws.co.at>
2022-06-20 00:00:38 +0200

    implemented  table creation unit tests

--- test/integration_tests/integration_tests.dart
+++ test/integration_tests/integration_tests.dart
@@ -1,8 +1,9 @@
 import 'package:test/test.dart';
 import 'package:postgres_migration/postgres_migration.dart';
+import './integration_test_data.dart';
 
 /// Collection of tests to run against DB
-/// 
+///
 /// Tests only check for thrown exceptions (if accepted by DB), NOT for actual results in DB
 void executeIntegrationTests(
   Future<dynamic> Function(String sqlStatement) callDB,
@@ -10,10 +11,52 @@ void executeIntegrationTests(
   final migrator = TableMigrator('table_name');
 
   // Remove table after every run
-  tearDownAll(() async => await callDB(migrator.removeTable()));
+  tearDown(() async => await callDB(migrator.removeTable(ifExists: true)));
 
-  test('Create simple table', () async {
-    final statement = migrator.createTable([]);
-    await callDB(statement);
+  group('1. Create Table //', () {
+    test('1. Simple table', () async {
+      final statement = migrator.createTable([]);
+      await callDB(statement);
+    });
+
+    test('2. Test numeric columns', () async {
+      for (final dataSet in numericColumns.values) {
+        final statement = migrator.createTable(dataSet);
+        await callDB(statement);
+        await callDB(migrator.removeTable());
+      }
+    });
+
+    test('3. Test text columns', () async {
+      for (final dataSet in textColumns.values) {
+        final statement = migrator.createTable(dataSet);
+        await callDB(statement);
+        await callDB(migrator.removeTable());
+      }
+    });
+
+    test('4. Test boolean columns', () async {
+      for (final dataSet in booleanColumns.values) {
+        final statement = migrator.createTable(dataSet);
+        await callDB(statement);
+        await callDB(migrator.removeTable());
+      }
+    });
+
+    test('5. Test uuid columns', () async {
+      for (final dataSet in uuidColumns.values) {
+        final statement = migrator.createTable(dataSet);
+        await callDB(statement);
+        await callDB(migrator.removeTable());
+      }
+    });
+
+    test('6. Test datetime columns', () async {
+      for (final dataSet in dateTimeColumns.values) {
+        final statement = migrator.createTable(dataSet);
+        await callDB(statement);
+        await callDB(migrator.removeTable());
+      }
+    });
   });
 }
