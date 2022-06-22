1f9fcc8511291404546df6d7509fc076646a5c6d
Felix Weickert <felix.weickert@dws.co.at>
2022-06-22 08:35:08 +0200

    improved documentation

--- lib/src/interfaces/table_property.dart
+++ lib/src/interfaces/table_property.dart
@@ -1,18 +1,17 @@
-/// Abstract class for defining parameters on tables
-/// 
-/// Parameters can be anything that is declared with a table, e.g. columns,constraints
+/// Abstract class for defining table properties
 ///
-/// ```CREATE TABLE example(parameter1, parameter2, parameter 3, etc)```
+/// Properties can be anything that is declared with a table, e.g. columns,constraints
+///
+/// ```CREATE TABLE example(property1, property2, property3, etc)```
 abstract class TableProperty {
   String get sqlSnippet;
 }
 
-
-/// Class for freely defining table parameters
-/// 
-/// Parameters can anything that is declared with a table, e.g. columns,constraints
+/// Class for freely defining table properties
 ///
-/// ```CREATE TABLE example(parameter1, parameter2, parameter 3, etc)```
+/// Properties can anything that is declared with a table, e.g. columns,constraints
+///
+/// ```CREATE TABLE example(property1, property2, property3, etc)```
 class ManualTableProperty implements TableProperty {
   String parameter;
 
