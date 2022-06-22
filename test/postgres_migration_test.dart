24d0c64bca3760037935ada6747403e6ebc8c7a9
Felix Weickert <felix.weickert@dws.co.at>
2022-06-22 10:38:30 +0200

    doc: how to create code coverage report

--- test/postgres_migration_test.dart
+++ test/postgres_migration_test.dart
@@ -3,6 +3,14 @@ import 'package:test/test.dart';
 import 'integration_tests/postgresql_integration_tests.dart';
 import 'unit_tests/unit_tests.dart';
 
+
+// Generate `coverage/lcov.info` file:
+// - flutter test --coverage
+// Generate HTML report (on macOS you need to have lcov installed on your system (`brew install lcov`))
+// - genhtml coverage/lcov.info -o coverage/html
+// Open the report
+// - open coverage/html/index.html
+
 void main() {
   group('Unit Tests', () {
     executeUnitTests();
